e03597ed87430e7e48add9367ec703b09d6cacf2
MalinaAlice <malinaalice18@gmail.com>
2025-03-29 19:10:07 +0200

    Add log lines on existing tests

--- src/test/java/pages/IndexPage.java
+++ src/test/java/pages/IndexPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import pageLocators.IndexLocators;
 
@@ -12,31 +13,41 @@ public class IndexPage extends BasePage{
 
     public void clickOnAlertWindowFrameMenu() {
         elementHelper.clickJsLocator(IndexLocators.alertsMenu);
+        LoggerUtility.infoLog("The user clicked on Alert Window Menu.");
     }
 
     public void clickOnAlertSubMenu() {
         elementHelper.clickJsLocator(IndexLocators.alertsPage);
+        LoggerUtility.infoLog("The user clicked on Alert Window SubMenu.");
     }
 
     public void clickOnFramesSubMenu() {
         elementHelper.clickJsLocator(IndexLocators.framesPage);
+        LoggerUtility.infoLog("The user clicked on Frame Window SubMenu.");
     }
 
     public void clickOnWindowsSubMenu() {
         elementHelper.clickJsLocator(IndexLocators.browserWindowPage);
+        LoggerUtility.infoLog("The user clicked on Windows SubMenu.");
     }
 
-    public void clickOnFormsMenu() { elementHelper.clickJsLocator(IndexLocators.formsMenu);}
+    public void clickOnFormsMenu() {
+        elementHelper.clickJsLocator(IndexLocators.formsMenu);
+        LoggerUtility.infoLog("The user clicked on Forms Window Menu.");
+    }
 
     public void clickOnPracticeFormSubMenu() {
         elementHelper.clickJsLocator(IndexLocators.practiceForm);
+        LoggerUtility.infoLog("The user clicked on Practice Form Window SubMenu.");
     }
 
     public void clickOnElementsMenu() {
         elementHelper.clickJsLocator(IndexLocators.elementsMenu);
+        LoggerUtility.infoLog("The user clicked on Elements Window Menu.");
     }
 
     public void clickOnWebTablesSubMenu() {
         elementHelper.clickJsLocator(IndexLocators.webTables);
+        LoggerUtility.infoLog("The user clicked on Web Tables Window SubMenu.");
     }
 }
--- src/test/java/pages/PracticeFormPage.java
+++ src/test/java/pages/PracticeFormPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import loggerUtility.LoggerUtility;
 import modelObject.PracticeFormModel;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +10,7 @@ import pageLocators.PracticeFormLocators;
 import java.io.File;
 import java.util.List;
 
-public class PracticeFormPage extends BasePage{
+public class PracticeFormPage extends BasePage {
 
     public PracticeFormPage(WebDriver driver) {
         super(driver);
@@ -17,10 +18,13 @@ public class PracticeFormPage extends BasePage{
 
     public void populateEntireForm(PracticeFormModel testData) {
         elementHelper.fillLocator(PracticeFormLocators.firstNameElement, testData.getFirstNameValue());
+        LoggerUtility.infoLog("The user fills First Name with value: " + testData.getFirstNameValue());
 
         elementHelper.fillLocator(PracticeFormLocators.lastNameElement, testData.getLastNameValue());
+        LoggerUtility.infoLog("The user fills Last Name with value: " + testData.getLastNameValue());
 
         elementHelper.fillLocator(PracticeFormLocators.emailElement, testData.getEmailValue());
+        LoggerUtility.infoLog("The user fills Email with value: " + testData.getEmailValue());
 
 
         List<WebElement> genderElement = elementHelper.getListElements(PracticeFormLocators.genderLocator);
@@ -35,12 +39,14 @@ public class PracticeFormPage extends BasePage{
                 elementHelper.clickJsLocator(genderElement.get(2));
                 break;
         }
+        LoggerUtility.infoLog("The user fills Gender with value: " + testData.getGenderValue());
 
         elementHelper.fillLocator(PracticeFormLocators.mobileNumberElement, testData.getMobileNumberValue());
 
-        for(String subject : testData.getSubjectValues()) {
+        for (String subject : testData.getSubjectValues()) {
             elementHelper.fillPressLocator(PracticeFormLocators.subjectElement, subject, Keys.ENTER);
         }
+        LoggerUtility.infoLog("The user choose these Subjects: " + testData.getSubjectValues());
 
         List<WebElement> hobbiesElement = elementHelper.getListElements(PracticeFormLocators.hobbiesLocator);
         for (WebElement hobby : hobbiesElement) {
@@ -48,19 +54,24 @@ public class PracticeFormPage extends BasePage{
                 elementHelper.clickJsLocator(hobby);
             }
         }
+        LoggerUtility.infoLog("The user choose these Hobbies: " + testData.getHobbyValues());
 
-        File file = new File (testData.getChooseFilePicture());
+        File file = new File(testData.getChooseFilePicture());
         elementHelper.fillLocator(PracticeFormLocators.chooseFileElement, file.getAbsolutePath());
+        LoggerUtility.infoLog("The user uploads the file: " + testData.getChooseFilePicture());
 
         elementHelper.fillLocator(PracticeFormLocators.currentAddressElement, testData.getCurrentAddressValue());
+        LoggerUtility.infoLog("The user's Address is: " + testData.getCurrentAddressValue());
 
         elementHelper.clickJsLocator(PracticeFormLocators.stateElement);
 
         elementHelper.fillPressLocator(PracticeFormLocators.chooseStateElement, testData.getChooseStateValue(), Keys.ENTER);
+        LoggerUtility.infoLog("The user's State is: " + testData.getChooseStateValue());
 
         elementHelper.clickJsLocator(PracticeFormLocators.cityElement);
 
         elementHelper.fillPressLocator(PracticeFormLocators.chooseCityElement, testData.getChooseCityValue(), Keys.ENTER);
+        LoggerUtility.infoLog("The user's City is: " + testData.getChooseCityValue());
 
         elementHelper.clickJsLocator(PracticeFormLocators.submitButton);
     }
@@ -73,35 +84,44 @@ public class PracticeFormPage extends BasePage{
 
         elementHelper.validateElementContainsText(tableContentList.get(0), "Student Name");
         elementHelper.validateElementContainsText(tableContentList.get(0), testData.getFirstNameValue() + ' ' + testData.getLastNameValue());
+        LoggerUtility.infoLog("Validate the First and Last Name: " + testData.getFirstNameValue() + " " + testData.getLastNameValue());
 
         elementHelper.validateElementContainsText(tableContentList.get(1), "Student Email");
         elementHelper.validateElementContainsText(tableContentList.get(1), testData.getEmailValue());
+        LoggerUtility.infoLog("Validate the Email: " + testData.getEmailValue());
 
         elementHelper.validateElementContainsText(tableContentList.get(2), "Gender");
         elementHelper.validateElementContainsText(tableContentList.get(2), testData.getGenderValue());
+        LoggerUtility.infoLog("Validate the Gender: " + testData.getGenderValue());
 
         elementHelper.validateElementContainsText(tableContentList.get(3), "Mobile");
         elementHelper.validateElementContainsText(tableContentList.get(3), testData.getMobileNumberValue());
+        LoggerUtility.infoLog("Validate the Mobile: " + testData.getMobileNumberValue());
 
         elementHelper.validateElementContainsText(tableContentList.get(5), "Subjects");
         String subjectValue = String.join(", ", testData.getSubjectValues());
         elementHelper.validateElementContainsText(tableContentList.get(5), subjectValue);
+        LoggerUtility.infoLog("Validate the Subjects: " + testData.getSubjectValues());
 
         elementHelper.validateElementContainsText(tableContentList.get(6), "Hobbies");
         String hobbyValue = String.join(", ", testData.getHobbyValues());
         elementHelper.validateElementContainsText(tableContentList.get(6), hobbyValue);
+        LoggerUtility.infoLog("Validate the Hobbies: " + testData.getHobbyValues());
 
         elementHelper.validateElementContainsText(tableContentList.get(7), "Picture");
         File file = new File(testData.getChooseFilePicture());
         String fileName = file.getName();
         elementHelper.validateElementContainsText(tableContentList.get(7), fileName);
+        LoggerUtility.infoLog("Validate the File: " + testData.getChooseFilePicture());
 
         elementHelper.validateElementContainsText(tableContentList.get(8), "Address");
         String addressValue = String.join(", ", testData.getCurrentAddressValue());
         elementHelper.validateElementContainsText(tableContentList.get(8), addressValue);
+        LoggerUtility.infoLog("Validate the Address: " + testData.getCurrentAddressValue());
 
 
         elementHelper.validateElementContainsText(tableContentList.get(9), "State and City");
         elementHelper.validateElementContainsText(tableContentList.get(9), testData.getChooseStateValue() + ' ' + testData.getChooseCityValue());
+        LoggerUtility.infoLog("Validate the State and City: " + testData.getChooseStateValue() + " " + testData.getChooseCityValue());
     }
 }
