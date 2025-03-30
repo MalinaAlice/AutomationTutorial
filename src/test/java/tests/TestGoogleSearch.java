983c81b2cd77f4efc27c6199fe711487516d02c6
MalinaAlice <malinaalice18@gmail.com>
2025-03-31 00:14:21 +0300

    Change Google Test

--- src/test/java/tests/TestGoogleSearch.java
+++ src/test/java/tests/TestGoogleSearch.java
@@ -1,9 +1,6 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
@@ -18,9 +15,9 @@ public class TestGoogleSearch {
 
     public void searchGoogle() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
-        options.addArguments("--headless=new");
 
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com/");
@@ -28,7 +25,9 @@ public class TestGoogleSearch {
 
         //Thread.sleep(2000);
         WebElement respingeTot = driver.findElement(By.id("W0wltc"));
-        respingeTot.click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", respingeTot);
+        //respingeTot.click();
 
         //Thread.sleep(1000);
         WebElement searchElement = driver.findElement(By.id("APjFqb"));
