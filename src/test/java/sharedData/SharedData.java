bece147b6060bf464499e4e4756400c7e62a9f8e
MalinaAlice <malinaalice18@gmail.com>
2025-04-01 19:01:44 +0300

    change the tests to run headless only on remote env

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -13,20 +13,26 @@ public class SharedData {
     private WebDriver driver;
     private String testName;
 
-    @BeforeMethod (alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void prepareEnvironment() {
         testName = this.getClass().getSimpleName();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
 
-        driver = new ChromeDriver(options);
+        String remoteEnv = System.getProperty("remote");
+        if (Boolean.parseBoolean(remoteEnv)) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            driver = new ChromeDriver(options);
+        } else {
+            driver = new ChromeDriver();
+        }
+
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         LoggerUtility.startTest(testName);
     }
 
-    @AfterMethod (alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void clearEnvironment() {
         driver.quit();
         LoggerUtility.finishTest(testName);
